3284244a0c619a97b0c3b1ad697445f6fa87a5f4
yuenfu001 <liuyuenfu@gmail.com>
2024-11-22 08:45:21 +0100

    changes in account.urls and setting

--- oms_project/settings.py
+++ oms_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'drf_yasg'
     'account',
     'forms_app',
     'base_app',
