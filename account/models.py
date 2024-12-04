326a243d30697f99f5dc790f70bfb3a8c71aa6d5
yuenfu001 <liuyuenfu@gmail.com>
2024-12-04 22:11:38 +0100

    Adjusted Admin model

--- account/models.py
+++ account/models.py
@@ -1,48 +1,38 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-
 class AccountUserManager(BaseUserManager):
-    """
-        This is a manager for Account class
-    """
-
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Users must have an Emaill address")
-        if not username:
-            raise ValueError("Users must have an Username")
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-
+            raise ValueError("The Email field must be set")
+        
+        # Ensure fields like 'is_superuser' are handled correctly
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-            username=username,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-class Account(AbstractBaseUser):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
-    phone_number = models.CharField(default=None,null=True,max_length=30)
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'  # Use email as the primary login field
     objects = AccountUserManager()
 
-
-    def __str__(self):
-        return self.email
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['first_name', 'last_name']
