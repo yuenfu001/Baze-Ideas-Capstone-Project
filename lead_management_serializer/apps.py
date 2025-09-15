08f32ba4ce002b5680be668a7038f3576799ce6f
Chiko Liu Abdulsamad <64754712+yuenfu001@users.noreply.github.com>
2025-09-15 21:14:17 +0100

    Delete lead_management_serializer directory

--- lead_management_serializer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeadManagementSerializerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lead_management_serializer'
--- lead_management_serializer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-app_name="lead_management_serializer"
-urlpatterns = [
-    path('enter-details',views.create_client_form, name='createform'),
-]
