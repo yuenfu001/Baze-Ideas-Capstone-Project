8d1f43e607b7955cde6547b0457bc69b57eadbff
Chiko Liu Abdulsamad <64754712+yuenfu001@users.noreply.github.com>
2025-09-15 21:15:00 +0100

    Delete account directory

--- account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Account
-# Register your models here.
-
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
-    list_display = [
-        'email','first_name','last_name'
-    ]
